661e3777be4725dd1e63b5974344bb81a195aba0
Leonidas Liakos <leonidas_liakos@yahoo.gr>
2018-11-12 17:56:08 +0200

    Initial commit

---
+++ data/settings.js
@@ -0,0 +1,49 @@
+var period={
+    minYear: 1950,
+    maxYear: 1975,
+}
+
+ var labels = {
+    pois: "Places",
+    grayscale: "Grayscale",
+    streets: "Streets",
+    datayear: "Data for year: ",
+    dataperiod:"Data for period: ",
+    points: "Points:" ,
+
+// birth fields
+	 aa: "aa" , 
+	 NOM_et_P_1:"NOM et Prénoms",
+	 NOM_et_Pr: "NOM_et_Pr" , 
+	 Fiches_du: "NOM et Prénoms" , 
+	 Numero_de: "Νumero de tombe" , 
+	 Grade: "Grade" , 
+	 Unité: "Unité" , 
+	 Bureau_de: "Bureau de recrutement" , 
+	 Recrutemen: "Recrutemen" , 
+	 Recrutem_1: "Recrutem_1" , 
+	 Date_de__1: "Date de Naissance" , 
+	 Lieu_Nai_1: "Lieu de Naissance" , 
+	 birth_pl_1: "birth_pl_1" , 
+	 birth_pl_2: "birth_pl_2" , 
+	 Bureau_d_1: "Département de Naissance" , 
+	 Date_de_D: "Date de Décès" , 
+	 Age: "Age" , 
+	 Cause_de_D: "Cause de Déces" , 
+	 Descriptio: "Description du décès" , 
+	 Descript_1: "Descript_1" , 
+	 Lieu_du__1: "Lieu du décès" , 
+         Lieu_du__2:  "Lieu_du__2",
+	 death_pl_1: "death_pl_1" , 
+	 death_pl_2: "death_pl_2" , 
+	 Lieu_du_de: "Lieu_du_de" , 
+	 Départeme: "Département de transcription du décès" , 
+	 burial_pla: "Pays de transcription du décès" , 
+	 Comments: "Comments" , 
+	 F27: "F27" , 
+	 F28: "F28" , 
+
+
+
+    
+};
